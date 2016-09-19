782fc2f849ff0c4aac817cb2053d42e89d4ee34a
Léo Baecker <leo.baecker@epitech.eu>
2016-09-18 21:18:27 -0400

    get each message

--- scraper.py
+++ scraper.py
@@ -1,21 +1,29 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 import os
 import math
+from datetime import datetime
 from variables import *
-from bs4 import BeautifulSoup
+from get_messages import *
+from bs4 import BeautifulSoup, SoupStrainer
 
-
+# Main Function
 def main(): 
 	s = requests.Session()
 	url1 = "http://www.jeuxvideo.com/forums/0-1000021-0-1-0-"
 	page = 1
 	url2 = "-0-communaute.htm"
-	url = url1 + str(page) + url2
-	topic_list = get25Topics(url, s)
-	fromLastPage(topic_list, s)
 
+	while 1:
+		url = url1 + str(page) + url2
+		print red + url + white
+		topic_list = get25Topics(url, s)
+		fromLastPage(topic_list, s)
+		return
+
+# Get the 25 topics from the page
 def get25Topics(url, s):
 	r = singleRequest(url, s)
 	if r != False:
@@ -40,25 +48,88 @@ def get25Topics(url, s):
 					topic_list.append('https://www.jeuxvideo.com' + topic_lastpage)
 	return topic_list
 
+# Get to the first page from the last one
 def fromLastPage(topic_list, s):
-	i = 0
 	for topic in topic_list:
-		previous = topic.split('-')
-		previous_page = int(previous[3]) - 1
-		if previous_page < 1:
-			return
-		previous[3] = str(previous_page)
-		topic = '-'.join(previous)
-		print cyan + topic + white
-		singleRequest(topic, s)
+		while int(topic.split('-')[3]) > 1:
+			previous = topic.split('-')
+			previous_page = int(previous[3]) - 1
+			previous[3] = str(previous_page)
+			topic = '-'.join(previous)
+			print cyan + topic + white
+			r = singleRequest(topic, s)
+			get_messages(r)
+
+# Parse each message
+def get_messages(page):
+	bloc_message = SoupStrainer('div', {'class': 'bloc-message-forum '})
+	soup = BeautifulSoup(page.text, "html.parser", parse_only=bloc_message)
+	for s in soup:
+		# PSEUDO
+		try:
+			pseudo = s.find('span', attrs={'class': 'bloc-pseudo-msg'})
+			pseudo = pseudo.getText().replace(' ', '').replace('\n', '')
+		except:
+			pseudo = "Pseudo supprimé"
+		# ANCRE
+		ancre = s['data-id']
+		# MESSAGE
+		message_raw = s.find('div', attrs={'class': 'text-enrichi-forum'})
+		message = message_raw.renderContents().replace('\n', '')
+		message_raw = message_raw.getText()
+		message_raw = ' '.join(message_raw.split())
+
+		nb_mots = len(message_raw.split(' '))
+		nb_chars = len(message_raw)
+
+		# DATE
+		date = s.find('div', attrs={'class': 'bloc-date-msg'}).text.replace('\n', '').replace('"', '').lstrip()
+		date = parse_date(date)
+
+		# AVATAR
+		try:
+			avatar = s.find('img', attrs={'class': 'user-avatar-msg'})
+			avatar = avatar['data-srcset'].replace('avatar-sm', 'avatar-md').replace('//image.jeuxvideo.com/', '') # Add 'image.jeuxvideo.com' in frontend
+		except:
+			avatar = "image.jeuxvideo.com/avatar-md/default.jpg"	
+		# print 'image.jeuxvideo.com/' + avatar
+		print ancre + ' ' + pseudo
+
+def parse_date(date):
+	p_date = date.split(' ')
+	# Encode characters
+	d = p_date[0]
+	p_date[1] = p_date[1].encode('utf8', 'replace')
+	if 'janvier' in p_date[1]:
+		m = '01'
+	elif 'février' in p_date[1]:
+		m = '02'
+	elif 'mars' in p_date[1]:
+		m = '03'
+	elif 'avril' in p_date[1]:
+		m = '04'
+	elif 'mai' in p_date[1]:
+		m = '05'
+	elif 'juin' in p_date[1]:
+		m = '06'
+	elif 'juillet' in p_date[1]:
+		m = '07'
+	elif 'août' in p_date[1]:
+		m = '08'
+	elif 'septembre' in p_date[1]:
+		m = '09'
+	elif 'octobre' in p_date[1]:
+		m = '10'
+	elif 'novembre' in p_date[1]:
+		m = '11'
+	elif 'décembre' in p_date[1]:
+		m = '12'
+	y = p_date[2]
+	h = p_date[4].split(':')
+	ladate = datetime(int(y), int(m), int(d), int(h[0]), int(h[1]), int(h[2]))
+	return ladate
 
 main()
-
-
-
-
-
-
 
 
 
